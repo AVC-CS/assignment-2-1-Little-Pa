0e1c1a2ee000effecd0ffc3c73758b3ed89fec06
Little-Pa <144716118+Little-Pa@users.noreply.github.com>
2023-10-26 13:18:55 -0700

    assignment 2-1

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+     m_perc = 40
+        f_perc = 60
+    
+        print (f' total number of students: \t {m_perc + f_perc}')
+        print (f' number of males and females: \t {m_perc} {f_perc}')
+        print (f' percentage of males and females: \t {m_perc: .2f} {f_perc: .2f}')
 
     """
     ########################################
